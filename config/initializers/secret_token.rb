8827c2af90cdb0bf545ccd13766086901e38a25f
Kauhsa <kauhsa@viuhka.fi>
2013-08-25 19:10:23 +0300

    Kaikki tiedostot Herokuun siirtoa varten

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ratebeer::Application.config.secret_token = '5e9e58fcd44f373496b085799bf8df9860287101bf0005ca5d0375f6c61ebbac99993519c7cc9efea2de11d42c8f545726e1a31eac6a43368f815b7591129998'
