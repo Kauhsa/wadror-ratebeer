8c36be3b3fce168241fb5f6c196348ef4c7a55a1
Kauhsa <kauhsa@viuhka.fi>
2013-09-13 15:31:59 +0300

    Korjaus validaattoriin

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -7,10 +7,12 @@ class Brewery < ActiveRecord::Base
 
   validates :name, :presence => true
   validates_numericality_of :year, { :greater_than_or_equal_to => 1042, 
-                                      :less_than_or_equal_to => Time.now.year, 
+                                      :less_than_or_equal_to => lambda {|_| Time.now.year}, 
                                       :only_integer => true }
 
   def to_s
     name
   end
+
+
 end
